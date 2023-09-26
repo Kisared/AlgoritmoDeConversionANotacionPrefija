a53fa9aa90996aa6e894a02b1ccbf38a77964fdd
Kisared <72580894+Kisared@users.noreply.github.com>
2023-09-26 08:13:28 -0600

    Añadir archivo principal
    Esta solo es la estructura del archivo principal, le falta analisis y empezar a implementar las funciones.

---
+++ algoritmo_convertir_prefijo.cpp
@@ -0,0 +1,25 @@
+/* 
+------------------------------------------------------------------
+Fecha: 26 sep. 2023
+
+Objetivo: Elaborar un programa que haga suma, resta, multiplicación 
+y división con funciones.
+
+Entradas: Seleccionar opcion de operación en menú y dos números flotantes
+para realizar la operación elegida. 
+
+Salidas: Resultado de la operación elegida. 
+
+Nombre: Omar Esteban Jiménez Legaspi
+Carrera: Lic. en Informática y Tecnologías Computacionales
+Semestre: 3
+Grupo: A
+------------------------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+}
